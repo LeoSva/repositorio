e060b9526f493b1e4c3311e659998ca716342259
LeoSva <leonaldo.sva@gmail.com>
2016-06-05 18:20:45 -0300

    ajuste

--- Projeto1/ejb/src/main/java/br/com/leo/business/TipoItemEJB.java
+++ Projeto1/ejb/src/main/java/br/com/leo/business/TipoItemEJB.java
@@ -1,7 +1,10 @@
 package br.com.leo.business;
 
+import javax.ejb.Local;
+
 import br.com.leo.entity.TipoItem;
 
+@Local
 public interface TipoItemEJB {
 	
 	public TipoItem findById( int idTipoItem ) throws Exception;
