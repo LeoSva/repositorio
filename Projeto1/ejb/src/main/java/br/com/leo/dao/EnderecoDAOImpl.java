e9f8664dc5e30af73ac9affcdcecf1220fa45c33
LeoSva <leonaldo.sva@gmail.com>
2016-06-05 17:04:59 -0300

    ajuste gui

--- Projeto1/ejb/src/main/java/br/com/leo/dao/EnderecoDAOImpl.java
+++ Projeto1/ejb/src/main/java/br/com/leo/dao/EnderecoDAOImpl.java
@@ -1,36 +1,17 @@
 package br.com.leo.dao;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.ejb.Stateless;
 
 import br.com.leo.entity.Endereco;
+import br.com.leo.util.BancoDadosUtil;
 
-@Stateless
+@Stateless(name="ejb/EnderecoDAO", mappedName="EnderecoDAOImpl")
 public class EnderecoDAOImpl implements EnderecoDAO{
-	
-	private static List<Endereco> listEndereco = new ArrayList<Endereco>();
-	
-	static{
-		
-		Endereco endereco = new Endereco();
-		
-		endereco.setIdEndereco(1);
-		endereco.setBairro( "Jardim Itapuã" );
-		endereco.setCep( "04188144" );
-		endereco.setCidade( "São Paulo" );
-		endereco.setEstado( "SP" );
-		endereco.setNumero( "199" );
-		endereco.setRua( "Jose Amaral do Patrocinio" );
-		
-		listEndereco.add( endereco );
-	}
 	
 	@Override
 	public Endereco findById( int idEndereco ) throws Exception{
 		
-		for ( Endereco endereco : listEndereco ) {
+		for ( Endereco endereco : BancoDadosUtil.getInstance().getListaEndereco() ) {
 			if( endereco.getIdEndereco() == idEndereco ){
 				return endereco;
 			}
@@ -43,10 +24,10 @@ public class EnderecoDAOImpl implements EnderecoDAO{
 	public void saveOrUpdate( Endereco endereco ) throws Exception{
 		
 		if( endereco.getIdEndereco() > 0 ){
-			listEndereco.remove( endereco );
+			BancoDadosUtil.getInstance().getListaEndereco().remove( endereco );
 		}
 		
-		listEndereco.add( endereco );
+		BancoDadosUtil.getInstance().getListaEndereco().add( endereco );
 	}
 
 }
